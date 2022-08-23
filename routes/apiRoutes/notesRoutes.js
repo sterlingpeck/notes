5a3e92eccad00ca3f08dcf949bf5309eb0860865
Sterling Peck <peck.sterling@gmail.com>
2022-08-23 11:55:01 -0600

    Create notes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,3 +1,19 @@
 const router = require("express").Router();
+const { createNote, validateNotes } = require("../../lib/notes");
+const notes = require("../../db/db.json");
+
+router.get("/notes", (req, res) => {
+  res.json(notes);
+});
+
+router.post("/notes", (req, res) => {
+  req.body.id = notes.length.toString();
+  if (!validateNotes(req.body)) {
+    res.status(400).send("Wrong format");
+  } else {
+    const note = createNote(req.body, notes);
+    res.json(note);
+  }
+});
 
 module.exports = router;
